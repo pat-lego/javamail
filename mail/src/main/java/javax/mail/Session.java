b46ced93c05299803526d4d2fcdec016232a239f
Patrique Legault <patrique.legault@gmail.com>
2024-08-28 20:30:10 -0400

    Adding uncommented code

--- mail/src/main/java/javax/mail/Session.java
+++ mail/src/main/java/javax/mail/Session.java
@@ -1182,10 +1182,10 @@ public final class Session {
 	};
 
 	// load default META-INF/javamail.default.address.map from mail.jar
-	//loadResource("/META-INF/javamail.default.address.map", cl, loader, true);
+	loadResource("/META-INF/javamail.default.address.map", cl, loader, true);
 
 	// load the META-INF/javamail.address.map file supplied by an app
-	//loadAllResources("META-INF/javamail.address.map", cl, loader);
+	loadAllResources("META-INF/javamail.address.map", cl, loader);
 
 	// load system-wide javamail.address.map from the
 	// <java.home>/{conf,lib} directory
